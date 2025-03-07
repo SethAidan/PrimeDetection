924ba449d4faf5a5a2909550d9b309560a29c58d
Seth Maxwell <seth.maxwell@student.manchester.ac.uk>
2025-03-07 01:31:36 +0000

    Call appropriate function depending on testMode

--- PrimeDetection.java
+++ PrimeDetection.java
@@ -9,7 +9,7 @@ import java.io.Console;
 import java.util.Arrays;
 
 class PrimeDetection{
-    public static void Main(String[] args){
+    public static void main(String[] args){
         // Declare global attributes
         String testMode;
 
@@ -19,10 +19,22 @@ class PrimeDetection{
         System.out.println("****************\n");
 
         // Choose testing mode - check primacy or generate primes
-        System.out.println("You may either : ")
+        System.out.println("You may either : ");
         System.out.println("check -> Enter a number and return whether is is prime, or,");
         System.out.println("generate -> Enter a range and receive all prime numbers in that range.\n");
-        testMode = inputStr(new String[]{"check","generate"}, "Would you like to 'check', or 'generate' : ");
+        testMode = inputStr(new String[]{"check","generate","arb"}, "Would you like to 'check', or 'generate' : ");
+
+        // Call the appropriate subroutine depending on testMode
+        switch(testMode){
+            case "check":
+                check();
+                break;
+            case "generate":
+                gen();
+                break;
+            default:
+                throw new UnacceptableInputException("User should not be able to enter a value other than 'check', or 'generate'");
+        }
         
     }
 
@@ -34,9 +46,16 @@ class PrimeDetection{
     public static String inputStr(String[] validInput, String inpQuestion){
         Console keyboardConsole = System.console();
         String inp;
+
+        //Ask the question until a valid answer is given
         do{
             inp = keyboardConsole.readLine(inpQuestion);
         } while(!Arrays.asList(validInput).contains(inp));
+
         return inp;
     }
+
+    public static void check(){}
+
+    public static void gen(){}
 }
